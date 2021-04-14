8d2ce3bfd8142aabfff94f757631dd129244bf02
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2021-04-14 10:32:16 +0800

    Day 2: Adding CSRF for submitting form

--- resources/views/trainings/create.blade.php
+++ resources/views/trainings/create.blade.php
@@ -9,6 +9,7 @@
 
                 <div class="card-body">
                     <form action="" method="POST">
+                        @csrf
                         <div class="form-group">
                             <label>Title</label>
                             <input type="text" name="title" class="form-control">

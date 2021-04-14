5a837f9a5ee79d80d9dbf66e20873b8b4748c267
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2021-04-14 09:42:03 +0800

    Day 2: adding template using default bootstrap

--- resources/views/trainings/index.blade.php
+++ resources/views/trainings/index.blade.php
@@ -1,5 +1,30 @@
-<h1>This is Training Index</h1>
+@extends('layouts.app')
 
-@foreach($trainings as $training)
-    {{ $training->id }} - {{ $training->title }} <br>
-@endforeach
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Training Index') }}</div>
+
+                <div class="card-body">
+                    <table class="table">
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Created At</th>
+                        </tr>
+                        @foreach($trainings as $training)
+                            <tr>
+                                <td>{{ $training->id }}</td>
+                                <td>{{ $training->title }}</td>
+                                <td>{{ $training->created_at }}</td>
+                            </tr>  
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
